b1d4337f93a3f091bcd9df4b8e27a7540e927b62
Surabhi Gogte <sgogte@purestorage.com>
2026-08-24 16:56:47 -0600

    nvme-tcp: refactor I/O queue setup path
    Split the I/O queue setup helpers apart so that the individual steps can
    be called directly from nvme_tcp_configure_io_queues():
    
    - Queue count negotiation moves out of nvme_tcp_alloc_io_queues() into a
      new nvme_tcp_io_queue_count(), leaving the allocator with just the
      per-queue allocation loop.
    - TLS PSK validation moves out of __nvme_tcp_alloc_io_queues() into a
      new nvme_tcp_tls_check_psk().
    - nvme_tcp_configure_io_queues() now calls the three steps directly in
      the same order as before.
    
    Signed-off-by: Surabhi Gogte <sgogte@purestorage.com>

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -2183,10 +2183,8 @@ out_free_queue:
 	return ret;
 }
 
-static int __nvme_tcp_alloc_io_queues(struct nvme_ctrl *ctrl)
+static int nvme_tcp_tls_check_psk(struct nvme_ctrl *ctrl)
 {
-	int i, ret;
-
 	if (nvme_tcp_tls_configured(ctrl)) {
 		if (ctrl->opts->concat) {
 			/*
@@ -2208,6 +2206,13 @@ static int __nvme_tcp_alloc_io_queues(struct nvme_ctrl *ctrl)
 		}
 	}
 
+	return 0;
+}
+
+static int __nvme_tcp_alloc_io_queues(struct nvme_ctrl *ctrl)
+{
+	int i, ret;
+
 	for (i = 1; i < ctrl->queue_count; i++) {
 		ret = nvme_tcp_alloc_queue(ctrl, i,
 				ctrl->tls_pskid);
@@ -2224,7 +2229,7 @@ out_free_queues:
 	return ret;
 }
 
-static int nvme_tcp_alloc_io_queues(struct nvme_ctrl *ctrl)
+static int nvme_tcp_io_queue_count(struct nvme_ctrl *ctrl)
 {
 	unsigned int nr_io_queues;
 	int ret;
@@ -2246,14 +2251,22 @@ static int nvme_tcp_alloc_io_queues(struct nvme_ctrl *ctrl)
 
 	nvmf_set_io_queues(ctrl->opts, nr_io_queues,
 			   to_tcp_ctrl(ctrl)->io_queues);
-	return __nvme_tcp_alloc_io_queues(ctrl);
+	return 0;
 }
 
 static int nvme_tcp_configure_io_queues(struct nvme_ctrl *ctrl, bool new)
 {
 	int ret, nr_queues;
 
-	ret = nvme_tcp_alloc_io_queues(ctrl);
+	ret = nvme_tcp_io_queue_count(ctrl);
+	if (ret)
+		return ret;
+
+	ret = nvme_tcp_tls_check_psk(ctrl);
+	if (ret)
+		return ret;
+
+	ret = __nvme_tcp_alloc_io_queues(ctrl);
 	if (ret)
 		return ret;
 
